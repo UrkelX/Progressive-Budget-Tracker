84b8734799ca5167dbfc74559579f0e7ef1f8c25
JordonMoses <jordon@blackbirdrevolt.com>
2021-02-23 18:58:17 -0600

    service worker updated and manifest

---
+++ public/service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.js",
+  "/index.html",
+  "/styles.css",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/manifest.webmanifest",
+];
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/transaction")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+    
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+         
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
